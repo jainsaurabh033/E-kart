ab4c910ee104b66f9e84179e1242ce6456563dee
saurabh <saurabhjain04r@gmail.com>
2023-05-19 02:18:24 +0530

    Login and sigup frontend is done

---
+++ frontend/src/Routes.js
@@ -0,0 +1,4 @@
+import LoginPage from "../src/pages/LoginPage.js";
+import SignupPage from "../src/pages/SignupPage.js";
+
+export { LoginPage, SignupPage };
---
+++ frontend/src/pages/SignupPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Signup from "../components/Signup/Signup.js";
+
+const SignupPage = () => {
+  return (
+    <div>
+      <Signup />
+    </div>
+  );
+};
+
+export default SignupPage;
