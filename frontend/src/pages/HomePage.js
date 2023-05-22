b70228be867d5f6c4a4068877682d3b480cf6143
saurabh <saurabhjain04r@gmail.com>
2023-05-22 21:20:09 +0530

    Hero frontend and catagory frontend done

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "../components/Layout/Header";
-// import Hero from "../components/Route/Hero/Hero";
-// import Categories from "../components/Route/Categories/Categories";
+import Hero from "../components/Route/Hero/Hero";
+import Categories from "../components/Route/Categories/Categories";
 // import BestDeals from "../components/Route/BestDeals/BestDeals";
 // import FeaturedProduct from "../components/Route/FeaturedProduct/FeaturedProduct";
 // import Events from "../components/Events/Events";
@@ -12,9 +12,9 @@ const HomePage = () => {
   return (
     <div>
       <Header activeHeading={1} />
-      {/* <Hero />
+      <Hero />
       <Categories />
-      <BestDeals />
+      {/* <BestDeals />
       <Events />
       <FeaturedProduct />
       <Sponsored />
