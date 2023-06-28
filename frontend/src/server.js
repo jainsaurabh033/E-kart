56f24bb190711c331f991041432455aed7ade88b
saurabh <saurabhjain04r@gmail.com>
2023-06-28 18:13:43 +0530

    update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://shop0-nine.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://shop0-nine.vercel.app/";
