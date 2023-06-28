978199e8152a33dbafe6bab9359d6bc326784902
saurabh <saurabhjain04r@gmail.com>
2023-06-28 21:45:10 +0530

    fixing issue

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/test", (req, res) => {
+app.use("/", (req, res) => {
   res.send("Hello world");
 });
 app.use("/", express.static(path.join(__dirname, "uploads")));
