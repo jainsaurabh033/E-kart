9919dd0d38e5ef4e5dabf91cdafe592366142846
saurabh <saurabhjain04r@gmail.com>
2023-07-03 23:58:30 +0530

    uncomment url

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,7 +1,7 @@
-// export const server = "https://shop0-bay.vercel.app/api/v2";
+export const server = "https://shop0-bay.vercel.app/api/v2";
 
-// export const backend_url = "https://shop0-bay.vercel.app/";
+export const backend_url = "https://shop0-bay.vercel.app/";
 
-export const server = "http://localhost:8000/api/v2";
+// export const server = "http://localhost:8000/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+// export const backend_url = "http://localhost:8000/";
